83eeeca4afca90c3346444456ce8b40d8a067d66
mrvalraven <akatiago@gmail.com>
2021-10-03 15:22:08 +0100

    added debugging

--- routes/email.js
+++ routes/email.js
@@ -1,23 +1,20 @@
-const router = require('express').Router();
-const { sendEmail } = require('../functions/emailFunctions')
+const router = require("express").Router();
+const { sendEmail } = require("../functions/emailFunctions");
 
-router.post('/sendEmail', async (req, res) => {
+router.post("/sendEmail", async (req, res) => {
+  const sendTo = req.body.sendTo;
+  const subject = req.body.subject;
+  const message = req.body.message;
 
-    const sendTo = req.body.sendTo;
-    const subject = req.body.subject;
-    const message = req.body.message;
-    
-    try {
-        let sendResult = await sendEmail(sendTo, subject, message)
+  try {
+    let sendResult = await sendEmail(sendTo, subject, message);
 
-        res.status(200).json({message: 'Email sent sucessfully'});
-        
-    } catch (error) {
-        res.status(400).json({message: 'Invalid parameters'});
-    }
+    res.status(200).json({ message: "Email sent sucessfully" });
+  } catch (error) {
+    res.status(400).json({ message: "Invalid parameters" });
+  }
 
+  console.log(sendResult);
 });
 
 module.exports = router;
-
-
