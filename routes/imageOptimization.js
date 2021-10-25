5dc15cb705903e09c90ace5fbfc8fe9d01f3d95c
mrvalraven <akatiago@gmail.com>
2021-10-25 17:11:22 +0100

    added tinify api

---
+++ routes/imageOptimization.js
@@ -0,0 +1,12 @@
+const tinify = require("tinify");
+const fs = require("fs");
+
+tinify.key = process.env.TINIFY_API_KEY;
+
+fs.readFile(fileName, (err, sourceData) => {
+  if (err) throw err;
+  tinify.fromBuffer(sourceData).toBuffer((err, resultData) => {
+    if (err) throw err;
+    // ...
+  });
+});
